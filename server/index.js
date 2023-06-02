34f2ea160a4c03d5a41e8820e175c8706915a7bb
saurabh <sauravvermas1097v@gmail.com>
2023-06-02 23:25:54 +0530

    connected with mongo and env config

--- server/index.js
+++ server/index.js
@@ -1,14 +1,32 @@
-import dotenv from "dotenv"
-import express from "express"
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
 const app = express();
+app.use(cors())
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send("jelskj")
 })
 
-app.listen(3000 () => {
-  console.log(`listening at http://localhost 3000`)
-})
+mongoose.connect(process.env.MONGO_URL,
+  { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+  }).then(() => {
+    console.log('Connected to MongoDB');
+  }).catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+
+
+
+// Start the server
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port http://localhost:${process.env.PORT}`);
+});
