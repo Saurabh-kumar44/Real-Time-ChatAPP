82e1f6a1b8e08ae8959e9b8b825cc485910419ed
saurabh <sauravvermas1097v@gmail.com>
2023-06-03 01:11:06 +0530

    styling the Registration form

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,33 +1,103 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import Logo from '../assets/logo.svg';
+import styled from 'styled-components'
 
 function Register() {
-    const handleSubmit = (event)=>{
+    const handleSubmit = (event) => {
         event.preventDefault();
         alert("form")
     }
-    const handleChange = (event)=>{
+    const handleChange = (event) => {
 
     }
-  return (
-    <>
-    <h1>register</h1>
-        <formContainer>
-            <form onSubmit={(event)=>{handleSubmit(event)}}>
-                <div className="brand">
-                    <img src="" alt="" />
-                    <h1>Snappy</h1>
-                </div>
-                <input type="text" placeholder='Username' name='username'onChange={e=>{handleChange(e)}} />
-                <input type="email" placeholder='Email' name='email'onChange={e=>{handleChange(e)}} />
-                <input type="password" placeholder='Password' name='password'onChange={e=>{handleChange(e)}} />
-                <input type="password" placeholder='Confirm password' name='confirmPassword'onChange={e=>{handleChange(e)}} />
-                <button type='submit'>Create User</button>
-                <span>  Already have an account? <Link to="/login">Login</Link></span>
-            </form>
-        </formContainer>
-    </>
-  )
+    
+
+    return (
+        <>
+            <FormContainer>
+                <form onSubmit={(event) => { handleSubmit(event) }}>
+                    <div className="brand">
+                        <img src={Logo} alt="Logo"style={{height:"5rem"}}/>
+                        <h1>Snappy</h1>
+                    </div>
+                    <input type="text" placeholder='Username' name='username' onChange={e => { handleChange(e) }} />
+                    <input type="email" placeholder='Email' name='email' onChange={e => { handleChange(e) }} />
+                    <input type="password" placeholder='Password' name='password' onChange={e => { handleChange(e) }} />
+                    <input type="password" placeholder='Confirm password' name='confirmPassword' onChange={e => { handleChange(e) }} />
+                    <button type='submit'>Create User</button>
+                    <span>  Already have an account? <Link to="/login">Login</Link></span>
+                </form>
+            </FormContainer>
+        </>
+    )
 }
 
+const FormContainer = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1rem;
+    align-items: center;
+    background-color: #131324;
+    .brand{
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+        justify-content: center;
+        img{
+            height: 5rem;
+        }
+        h1{
+            color: white;
+        }
+    }
+    form{
+        display: flex;
+        flex-direction: column;
+        gap: 1.5rem;
+        background-color: #00000076;
+        border-radius: 2rem;
+        padding: 3rem 5rem;
+        input{
+            background-color: transparent;
+            padding: 0.5rem;
+            border: 0.1rem solid #4e0eff;
+            border-radius: 0.5rem;
+            color: white;
+            width: 100%;
+            font-size: 1rem;
+            &:focus{
+                border: 0.1rem solid #997af0;
+                outline: none;
+            }
+        }
+        button{
+            background-color:#9971f0;
+            padding: 0.5rem;
+            border-radius: 1rem 2rem;
+            font-weight: bold;
+            text-transform: uppercase;
+            cursor: pointer;
+            &:hover{
+                background-color: #4e0eff;
+            }
+        }
+        span{
+            color: white;
+            text-transform: uppercase;
+            a{
+                color: #4e0eff;
+                text-transform: uppercase;
+                text-decoration: none;
+                font-weight: bold;
+            }
+        }
+    }
+;
+`
+
 export default Register;
+
