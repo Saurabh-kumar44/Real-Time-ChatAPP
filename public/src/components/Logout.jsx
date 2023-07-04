4f5fc0cf73f180df2b48f35072ebfe5e1da76896
saurabh <sauravvermas1097v@gmail.com>
2023-07-04 10:27:17 +0530

    styling logout button

--- public/src/components/Logout.jsx
+++ public/src/components/Logout.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { FaPowerOff } from 'react-icons/fa'
 
 function Logout() {
-  return (
-    <>
-        <h3>Logout</h3>
-    </>
-  )
+    const navigate = useNavigate();
+    const handleClick = () => {
+        localStorage.clear();
+        navigate('/');
+    }
+    return (
+        <>
+            <Button onClick={handleClick} >
+                <FaPowerOff />
+            </Button>
+        </>
+    )
 }
+const Button = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0.3rem;
+    border-radius: 0.5rem;
+    background-color: #9a86f3;
+    border: none;
+    cursor: pointer;
+    svg{
+        font-size:1.3rem;
+        color: #ebe7ff;
+    }
+`;
 
-export default Logout
+export default Logout;
