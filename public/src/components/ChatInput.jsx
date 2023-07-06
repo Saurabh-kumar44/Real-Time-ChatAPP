cf1cec02afa400d3bf2d54be078b815e10c6e8f9
saurabh <sauravvermas1097v@gmail.com>
2023-07-06 15:25:11 +0530

    installing emoji mart

--- public/src/components/ChatInput.jsx
+++ public/src/components/ChatInput.jsx
@@ -2,26 +2,34 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { BsSendFill } from 'react-icons/bs';
 import { BsEmojiSmileFill } from 'react-icons/bs';
-import Picker from 'emoji-picker-react'
+import { Picker } from 'emoji-mart'
 
 function ChatInput() {
   const [showEmojiPicker, setShowEmojiPicker] = useState(false);
-  const handleEmojiPicker = () => {
+  const [msg, setMsg] = useState("");
+
+  const handleEmojiPickerHideShow = () => {
     setShowEmojiPicker(!showEmojiPicker);
+  }
+  const handleEmojiClick = (event, emoji) => {
+    console.log(emoji.emoji);
+    let message = msg;
+    message += emoji.emoji;
+    setMsg(message);
   }
 
   return (
     <Container>
       <div className="button-container">
         <div className="emoji">
-          <BsEmojiSmileFill onClick={handleEmojiPicker}/>
+          <BsEmojiSmileFill onClick={handleEmojiPickerHideShow} />
           {
-            showEmojiPicker && <Picker/>
+            showEmojiPicker && <Picker onEmojiClick={handleEmojiClick} />
           }
         </div>
       </div>
       <form className="input-container">
-        <input type="text" className="input" placeholder="Type your message here!" />
+        <input type="text" className="input" placeholder="Type your message here!" value={msg} onChange={(e) => { setMsg(e.target.value) }} />
         <button className="submit">
           <BsSendFill />
         </button>
@@ -44,32 +52,19 @@ const Container = styled.div`
     align-items: center;
     color: white;
     gap: 1rem;
-
     .emoji {
       position: relative;
-
+      bottom: 472px;
+      left: 200px;
       svg {
         cursor: pointer;
         font-size: 1.5rem;
         color: yellow;
       }
-    }
-  }
-
-  input {
-    width: 90%;
-    height: 50%;
-    color: white;
-    background-color: transparent;
-    border: none;
-    padding: 0.4rem;
-    padding-left: 0.1rem;
-    font-size: 1.2rem;
-    &::selection {
-      background-color: #9186f3;
-    }
-    &:focus {
-      outline: none;
+      .emoji-picker-react{
+        position: absolute;
+        top: -350px;
+      }
     }
   }
 
@@ -82,25 +77,42 @@ const Container = styled.div`
     gap: 1rem;
     background-color: #ffffff34;
     border: transparent;
-  }
-
-  .submit {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #9a86f3;
-    color: white;
-    border: none;
-    border-radius: 30%;
-    width: 4rem;
-    height: 2.5rem;
-    cursor: pointer;
-
-    svg {
-      font-size: 1.7rem;
+    input {
+      width: 90%;
+      height: 50%;
       color: white;
+      background-color: transparent;
+      border: none;
+      padding: 0.4rem;
+      padding-left: 0.1rem;
+      font-size: 1.2rem;
+      &::selection {
+        background-color: #9186f3;
+      }
+      &:focus {
+        outline: none;
+      }
+    }
+    .submit {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: #9a86f3;
+      color: white;
+      border: none;
+      border-radius: 30%;
+      width: 4rem;
+      height: 2.5rem;
+      cursor: pointer;
+  
+      svg {
+        font-size: 1.7rem;
+        color: white;
+      }
     }
   }
+
+  
 `;
 
 export default ChatInput;
